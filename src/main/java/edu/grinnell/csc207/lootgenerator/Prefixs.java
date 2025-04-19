20da4e13bcc15560bb79de9423969781e815179c
tangyixu <tangyixu@grinnell.edu>
2025-04-18 22:36:53 -0500

    Complete Affix

---
+++ src/main/java/edu/grinnell/csc207/lootgenerator/Prefixs.java
@@ -0,0 +1,41 @@
+package edu.grinnell.csc207.lootgenerator;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Tiffany
+ */
+public class Prefixs {
+
+    private ArrayList<String> prefixL = new ArrayList<>();
+    private ArrayList<String> modCode = new ArrayList<>();
+    private ArrayList<Pair> modRange = new ArrayList<>();
+
+    public Prefixs(String file) {
+        Scanner scan = new Scanner(file);
+        while (scan.hasNext()) {
+            String wholeS = scan.nextLine();
+            String[] eachS = wholeS.split("\t");
+            this.prefixL.add(eachS[0]);
+            this.modCode.add(eachS[1]);
+            this.modRange.add(new Pair(Integer.parseInt(eachS[2]),
+                    Integer.parseInt(eachS[3])));
+        }
+        scan.close();
+    }
+
+    public ArrayList<String> getPrefixL() {
+        return this.prefixL;
+    }
+
+    public ArrayList<String> getModCode() {
+        return this.modCode;
+    }
+
+    public ArrayList<Pair> getModRange() {
+        return this.modRange;
+    }
+
+}
