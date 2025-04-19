84995f5d3da8666ce2326d3e610db1c946bddf82
tangyixu <tangyixu@grinnell.edu>
2025-04-18 23:53:58 -0500

    Update tests

--- src/test/java/edu/grinnell/csc207/lootgenerator/Tests.java
+++ src/test/java/edu/grinnell/csc207/lootgenerator/Tests.java
@@ -1,5 +1,39 @@
 package edu.grinnell.csc207.lootgenerator;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.Test;
+
 public class Tests {
-    // TODO: include your unit tests here!
+
+    @Test
+    public void extractSuffix() {
+        String path = "data/small/MagicSuffix.txt";
+        Suffixs suf = new Suffixs(path);
+        assertEquals(5, suf.getSuffixL().size());
+    }
+
+    @Test
+    public void extractPrefix() {
+        String path = "data/small/MagicPrefix.txt";
+        Prefixs suf = new Prefixs(path);
+        assertEquals(5, suf.getPrefixL().size());
+    }
+
+    @Test
+    public void armorStats() {
+        String path = "data/small/monstats.txt";
+        Armors armors = new Armors(path);
+        int val = armors.getList().get(0).baseStats();
+        assertEquals(val >= 8 && val <= 10, "Invalid range for armor stats.");
+    }
+
+    @Test
+    public void isTC() {
+        Treasure t = new Treasure("armo3", new String[]{"Quilted Armor",
+            "Buckler", "Leather Armor"});
+        Treasures allT = new Treasures("data/small/TreasureClass.txt");
+        assertTrue(allT.isTC(t));
+    }
+
 }
